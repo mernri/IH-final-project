388a46de8eeea3089aac7e002e0a92aa902d6bd7
Rita Mernissi <mernissi.rita@gmail.com>
2019-03-21 01:35:44 +0100

    adding workspaces dynamically + listingCards ok

--- client-everymote/src/components/pages/ListingPage/Listing/Listing.js
+++ client-everymote/src/components/pages/ListingPage/Listing/Listing.js
@@ -1,53 +1,55 @@
-// import React, { Component } from "react";
-// import axios from "axios";
-// import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-// import ListingCard from "./ListingCard.js";
-// import "bulma/css/bulma.css";
+import ListingCard from "./ListingCard.js";
+import "bulma/css/bulma.css";
 
+class Listing extends Component {
+  constructor() {
+    super();
+    this.state = { listOfWorkspaces: [] };
+  }
 
-// class Listing extends Component {
-//   constructor() {
-//     super();
-//     this.state = { listOfWorkspaces: [] };
-//   }
+  getAllWorkspaces = () => {
+    axios
+      .get(`http://localhost:5000/api/workspaces`)
+      .then(responseFromApi => {
+        this.setState({
+          listOfWorkspaces: responseFromApi.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-//   getAllWorkspaces = () => {
-//     // axios.get(`http://localhost:5000/api/workspaces`)  envoi comme réponse un tableau
-//     axios.get(`http://localhost:5000/api/workspaces`).then(responseFromApi => {
-//       this.setState({
-//         listOfWorkspaces: responseFromApi.data
-//       });
-//     });
-//   };
+  componentDidMount() {
+    this.getAllWorkspaces();
+  }
 
-//   render() {
-//     return (
-//       <div>
+  render() {
+    return (
+      <div>
+        <div>
+          {this.state.listOfWorkspaces.map(workspace => {
+            return (
+              <div key={workspace._id}>
+                <Link to={`/workspace/${workspace._id}`}>
+                  <div style={{ width: "35%", float: "left", padding: "15px" }}>
+                    <ListingCard {...workspace} />
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+        <div style={{ width: "40%", float: "right" }} />
 
-//         <div>
-//           {this.state.listOfWorkspaces.map(workspace => {
-//             return (
-//               <div key={workspace._id}>
-//                 <Link to={`/workspaces/${workspace._id}`}>
-//                   <h3>{workspace.name}</h3>
-//                 </Link>
-//                 {/* <p style={{maxWidth: '400px'}} >{workspace.description} </p> */}
-//               </div>
-//             );
-//           })}
-//         </div>
-//         <div style={{ width: "40%", float: "right" }}>
-//           <ListingCard
-//             name={this.workspace.name}
-//             getData={() => this.getAllWorkspaces()}
-//           />
-//         </div>
+        {console.log("this state list = " + this.state.listOfWorkspaces)}
+      </div>
+    );
+  }
+}
 
-//         {console.log(this.state.listOfWorkspaces)}
-//       </div>
-//     );
-//   }
-// }
-
-// export default Listing;
+export default Listing;
--- client-everymote/src/components/pages/ListingPage/ListingPage.js
+++ client-everymote/src/components/pages/ListingPage/ListingPage.js
@@ -1,19 +1,24 @@
 import React from "react";
+import WorkspaceListing from "../ListingPage/Listing/Listing.js";
 
 class ListingPage extends React.Component {
   render() {
     var style1 = {
-        background: 'violet',
-      };
-      var style2 = {
-        background: 'pink',
-      };
+      background: "white"
+    };
+    var style2 = {
+      background: "pink"
+    };
 
     return (
       <div>
         <div className="columns">
-          <div className="column is-three-fifths" style={style1}> hello I'm a listing</div>
-          <div className="column is-two-fifths" style={style2}> Hello I'm a map</div>
+          <div className="column is-three-fifths" style={style1}>
+            <WorkspaceListing />
+          </div>
+          <div className="column is-two-fifths" style={style2}>
+            Hello I'm a map
+          </div>
         </div>
       </div>
     );
